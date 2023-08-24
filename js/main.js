2ae3947cd5a89dd5657ecf9ca48edf99094e505f
umbertofaenza <umberto.faenza@gmail.com>
2023-08-24 15:19:59 +0200

    feat_js: Milestone 0 - Create array of objects

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// create array of objects
+const ourTeam = [
+  {
+    name: "Wayne Barnett",
+    role: "Founder & CEO",
+    pic: "wayne-barnett-founder-ceo.jpg",
+  },
+  {
+    name: "Angela Caroll",
+    role: "Chief Editor",
+    pic: "angela-caroll-chief-editor.jpg",
+  },
+  {
+    name: "Walter Gordon",
+    role: "Office Manager",
+    pic: "walter-gordon-office-manager.jpg",
+  },
+  {
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+    pic: "angela-lopez-social-media-manager.jpg",
+  },
+  {
+    name: "Scott Estrada",
+    role: "Developer",
+    pic: "scott-estrada-developer.jpg",
+  },
+  {
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+    pic: "barbara-ramos-graphic-designer.jpg",
+  },
+];
