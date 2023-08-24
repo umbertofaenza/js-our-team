fbb0318ba71f292b398795b1238e2b33d3303fa1
umbertofaenza <umberto.faenza@gmail.com>
2023-08-24 15:41:22 +0200

    feat_js: Milestone 1 - print info for each member

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // create array of objects
 const ourTeam = [
-  {
+  (member1 = {
     name: "Wayne Barnett",
     role: "Founder & CEO",
     pic: "wayne-barnett-founder-ceo.jpg",
-  },
+  }),
   {
     name: "Angela Caroll",
     role: "Chief Editor",
@@ -31,3 +31,12 @@ const ourTeam = [
     pic: "barbara-ramos-graphic-designer.jpg",
   },
 ];
+
+// print on console: name, role and pic(string) for each member of the team
+for (const member of ourTeam) {
+  for (let memberProps in member) {
+    console.log(member[memberProps]);
+  }
+}
+
+// !TODO: print the same info on the DOM as strings
